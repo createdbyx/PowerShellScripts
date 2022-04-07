d75b08afb913e68578e53746266aaa48f877cd4b
Dean <deanlunz@createdbyx.com>
2022-04-07 02:39:00 -0700

    code cleanup

--- Find Unpublished Git Repos.ps1
+++ Find Unpublished Git Repos.ps1
@@ -1,4 +1,5 @@
-﻿class GitData {
+﻿class GitData
+{
     [string]$Folder
     [string]$Branch
     [int]$Modifications
@@ -8,39 +9,60 @@
     [int]$Renamed
 }
 
-$drive = Read-Host "Enter Path or leave blank for current path"
-$gitFolders = (Get-ChildItem $drive -Attributes Directory+Hidden -ErrorAction SilentlyContinue -Filter ".git" -Recurse | select -Property FullName ) 
+$drive = Get-Location 
+$gitFolders = (Get-ChildItem $drive -Attributes Directory+Hidden -ErrorAction SilentlyContinue -Filter ".git" -Recurse | Select-Object -Property FullName)
 
 $results = New-Object System.Collections.ArrayList
 
-foreach( $entry in $gitFolders )
- {
+foreach ($entry in $gitFolders)
+{
     $e = $entry.FullName -replace ".{4}$"
-    cd $e     
-    [array] $tmp = git remote
-   [array] $status = git status -sb
-   $c = New-Object -TypeName GitData 
-   $c.Folder = $e
-  
-  foreach($x in $status)
-   {
-        $firstTwo= $x.Substring(0,2)
-       switch ( $firstTwo ) 
-        {
-            '##' { $c.Branch = $x }
-            ' M' { $c.Modifications += 1 }
-            ' A' { $c.Additions += 1 }
-            ' D' { $c.Deletions += 1 }
-            ' R' { $c.Renamed += 1 } 
-            'M ' { $c.Modifications += 1 }
-            'A ' { $c.Additions += 1 }
-            'D ' { $c.Deletions += 1 }
-            'R ' { $c.Renamed += 1 } 
-            '??' { $c.Untracked += 1 } 
-        }
-   } 
-   
-   $results.Add($c) | Out-Null
-}  
+    Set-Location $e
+    git remote | Out-Null
+    [array]$status = git status -sb
+    $c = New-Object -TypeName GitData
+    $c.Folder = $e
 
-$results | Sort-Object -Property @{Expression={$_.Modifications + $_.Additions + $_.Deletions + $_.Renamed}; Descending=$true} | ConvertTo-Html | Out-File -FilePath ($drive + "GitData.html")
+    foreach ($x in $status)
+    {
+        $firstTwo = $x.Substring(0, 2)
+        switch ($firstTwo)
+        {
+            '##' {
+                $c.Branch = $x
+            }
+            ' M' {
+                $c.Modifications += 1
+            }
+            ' A' {
+                $c.Additions += 1
+            }
+            ' D' {
+                $c.Deletions += 1
+            }
+            ' R' {
+                $c.Renamed += 1
+            }
+            'M ' {
+                $c.Modifications += 1
+            }
+            'A ' {
+                $c.Additions += 1
+            }
+            'D ' {
+                $c.Deletions += 1
+            }
+            'R ' {
+                $c.Renamed += 1
+            }
+            '??' {
+                $c.Untracked += 1
+            }
+        }
+    }
+
+    $results.Add($c) | Out-Null
+}
+
+$results | Sort-Object -Property @{ Expression = { $_.Modifications + $_.Additions + $_.Deletions + $_.Renamed }; Descending = $true }   
+Set-Location $drive
